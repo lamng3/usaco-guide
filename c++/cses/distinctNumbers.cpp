05ed6a953dcc6a17da33ec90f16a42adbf953a99
ltn18 <nguyentamhoanglam@gmail.com>
2025-08-16 14:18:21 -0500

    cses problem test
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- c++/cses/distinctNumbers.cpp
+++ c++/cses/distinctNumbers.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 void solve() {
-    
+    int n; cin >> n;
+    unordered_set<int> s;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << '\n';
 }
 
 int main() {
