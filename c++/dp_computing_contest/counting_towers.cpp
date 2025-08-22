efb52585665e875b292d5495731e4db810806387
ltn18 <nguyentamhoanglam@gmail.com>
2025-08-22 12:51:56 -0400

    counting towers cses dp
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- c++/dp_computing_contest/counting_towers.cpp
+++ c++/dp_computing_contest/counting_towers.cpp
@@ -4,32 +4,28 @@ Problem url: https://cses.fi/alon/task/2413
 */
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
-const ll MOD = 1e9+7;
+
+const ll mod = 1e9+7;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-
-    int tt; 
-    cin >> tt;
-    vector<int> qs(tt);
-    int maxn = 0;
-    for (int i = 0; i < tt; ++i) {
-        cin >> qs[i];
-        maxn = max(maxn, qs[i]);
+    int tt; cin >> tt;
+    
+    int q[tt], maxn = 0;
+    for (int i = 0; i < tt; i++) {
+        cin >> q[i];
+        maxn = max(maxn, q[i]);
+    }
+    
+    vector<ll> sep(maxn+1, 0), join(maxn+1, 0);
+    sep[1] = join[1] = 1;
+    for (int i = 2; i <= maxn; i++) {
+        sep[i] = (4 * sep[i-1] + join[i-1]) % mod;
+        join[i] = (2 * join[i-1] + sep[i-1]) % mod;
     }
 
-    vector<ll> join(maxn + 1, 0), sep(maxn + 1, 0);
-    join[1] = sep[1] = 1;
-    for (int i = 2; i <= maxn; ++i) {
-        sep[i]  = (4 * sep[i-1] + join[i-1]) % MOD;
-        join[i] = (2 * join[i-1] + sep[i-1]) % MOD;
+    for (int n : q) {
+        cout << ((join[n] + sep[n]) % mod) << '\n';
     }
-
-    for (int n : qs) {
-        cout << ((join[n] + sep[n]) % MOD) << '\n';
-    }
-
-    return 0;
 }
