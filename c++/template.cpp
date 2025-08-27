5864e806aac86b7d3000058b6c8a056a9d7fc810
ltn18 <nguyentamhoanglam@gmail.com>
2025-08-27 15:49:07 -0400

    template
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- c++/template.cpp
+++ c++/template.cpp
@@ -1,3 +1,4 @@
+// problem url: 
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,7 +8,7 @@ void solve() {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(nullptr);
+    cin.tie(0);
     int tt;
     cin >> tt;
     while (tt--) solve();
