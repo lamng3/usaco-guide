d16e2194f1cf819a4803c819962fe6a8ae731e6f
ltn18 <nguyentamhoanglam@gmail.com>
2025-08-29 22:32:12 -0400

    fix template
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- c++/template.cpp
+++ c++/template.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 void solve() {
-    
+
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int tt;
-    cin >> tt;
+    int tt = 1;
+    // cin >> tt;
     while (tt--) solve();
     return 0;
 }
