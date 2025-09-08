b749ca437eb3b16960ba7353720c573b1f685e16
ltn18 <nguyentamhoanglam@gmail.com>
2025-09-07 21:41:27 -0500

    usaco upsolve range dp
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

---
+++ c++/upsolve/usaco_upsolve/modern_art_3.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector< vector<int> > vii;
+
+const int oo = 305;
+
+void solve() {
+    int n; cin >> n;
+    vi p(n+1, 0); for (int i = 1; i <= n; i++) cin >> p[i];
+    vii dp(n+1, vi(n+1, oo));
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == j) dp[i][j] = 1;
+            else dp[i][j] = oo;
+        }
+        if (i != n) dp[i][i+1] = 1 + (p[i] != p[i+1]);
+    }
+    for (int gap = 2; gap < n; gap++) {
+        for (int pos = 1; pos + gap <= n; pos++) {
+            dp[pos][pos+gap] = dp[pos+1][pos+gap-1] + (p[pos] == p[pos+gap] ? 1 : 2);
+            for (int point = pos + 1; point < n; point++) {
+                dp[pos][pos+gap] = min(
+                    dp[pos][pos+gap],
+                    dp[pos][point] + dp[point][pos+gap] - 1
+                );
+            }
+        }
+    }
+    cout << dp[1][n] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
